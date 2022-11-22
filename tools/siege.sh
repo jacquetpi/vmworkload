57f688f92797c30c2d84b6a972e2365b9c25fa63
Pierre Jacquet <44034432+jacquetpi@users.noreply.github.com>
2022-11-22 13:41:11 +0100

    fix misc

--- tools/siege.sh
+++ tools/siege.sh
@@ -7,15 +7,5 @@ fi
 timeout="$2"
 concurrent="$3"
 fullip=$( tools/retrieveip.sh $1 );
-if [[ ${fullip} == *":"* ]];
-then
-  ip=$(echo $fullip | cut -d : -f 1)
-  port=$(echo $fullip | cut -d : -f 2)
-  # temporary workaround
-  ssh vmtornado@"${ip}" -p "$port" -o StrictHostKeyChecking=no -L ${port}:localhost:80 "sleep ${timeout}"  &
-  siege --time="$timeout"s --concurrent="$concurrent" --delay=1 http://localhost:${port}/
-else
-  ip="$fullip"
-  port="5432"
-  siege --time="$timeout"s --concurrent="$concurrent" --delay=1 http://"$vm_ip"/
-fi
+sed -i -- "s/true/false/g" ${HOME}/.siege/siege.conf
+siege --time="$timeout"s --concurrent="$concurrent" --delay=1 http://${fullip}/
